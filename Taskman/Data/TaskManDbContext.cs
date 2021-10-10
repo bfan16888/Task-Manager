0dff809c8e6128c7805a28d4292b0108f4391af4
brianwux <brianwux@BRIANWUX-MOBL>
2021-10-09 18:14:06 -0700

    Add project files.

---
+++ Taskman/Data/TaskManDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Taskman.Models;
+
+namespace Taskman.Data
+{
+    public class TaskManDbContext : DbContext
+    {
+        public TaskManDbContext(DbContextOptions<TaskManDbContext> options) : base(options)
+        {}
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<EmployeeTask>()
+            //    .HasOne(t => t.Employee)
+            //    .WithMany(e => e.Tasks)
+            //    .HasForeignKey(t=>t.EmployeeId);
+        }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<EmployeeTask> EmployeeTasks { get; set; }
+    }
+}
---
+++ Taskman/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Taskman.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime HireDate { get; set; }
+        public List<EmployeeTask> Tasks { get; set; }
+    }
+}
